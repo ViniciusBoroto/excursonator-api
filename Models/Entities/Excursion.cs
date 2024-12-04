21d0db636d6e0d9c684692a9aae749aa33f7da3a
Matheus Alves <matheus.alves356@outlook.com>
2024-12-04 00:16:25 -0300

    modelagem db

--- Models/Entities/Excursion.cs
+++ Models/Entities/Excursion.cs
@@ -10,5 +10,14 @@ namespace ExcursonatorAPI.Models.Entities
         public DateTime ReturnDate { get; set; }
         public string PicUrl { get; set; } = "https://www.flaticon.com/free-icon/image_739249";
         public required String Local { get; set; }
+
+        public string Title {get;set}
+
+        public string Theme {get;set}
+
+        public string Description {get;set}
+
+        public Itinerary {get;set}
+
+        public ImageUrl {get;set}
     }
-}
